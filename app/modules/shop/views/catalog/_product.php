3e3d0d5949a4c39695a6c80a10dc607c1a4a56f2
Egor Pisarev <egor.pisarev@gmail.com>
2015-06-04 17:31:11 +0600

    Добавлен контейнер для изображений товаров в списке товаров

--- app/modules/shop/views/catalog/_product.php
+++ app/modules/shop/views/catalog/_product.php
@@ -6,7 +6,9 @@ use yii\helpers\Markdown;
 <?php
 $images = $model->images;
 if (isset($images[0])) {
+    echo '<div class="image">';
     echo  Html::a(Html::img($images[0]->getUrl(),['width'=>'199px']),['/shop/catalog/view','slug'=>$model->slug,'category'=>$model->getCategorySlug()]);
+    echo '</div>';
 }
 $quantity=0;
 $position = Yii::$app->cart->getPositionById($model->id);
