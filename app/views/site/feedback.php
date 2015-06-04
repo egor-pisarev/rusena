1990f3595b2707bd6b1626725ed2cd6afadb7dde
Egor Pisarev <egor.pisarev@gmail.com>
2015-06-04 14:25:39 +0600

    Добавлена карта 	modified:   app/views/site/feedback.php

--- app/views/site/feedback.php
+++ app/views/site/feedback.php
@@ -10,7 +10,17 @@ use yii\captcha\Captcha;
 <section>
     <h1><?=$page->title?></h1>
     <div class="page">
-<?=$page->text?>
+
+        <div id="contacts-block">
+            <?=$page->text?>
+        </div>
+
+        <div id="map-block">
+            <h2>СХЕМА ПРОЕЗДА</h2>
+            <div id="map-container">
+                <script type="text/javascript" charset="utf-8" src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=Btn03cuIyjpFHBIknATPteyiTaCAp9yO&width=870&height=450"></script>
+            </div>
+        </div>
 
 <div id="callback-form">
     <h2>Обратная связь</h2>
