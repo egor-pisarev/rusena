7babde34f2bb82fdd5bef527f15d0a660395ec16
Egor Pisarev <egor.pisarev@gmail.com>
2015-06-14 22:02:32 +0600

    Добавлен текстовый блок метрики

--- app/themes/rusena/layouts/main.php
+++ app/themes/rusena/layouts/main.php
@@ -7,7 +7,7 @@ use app\modules\shop\widgets\Cart;
 use app\modules\shop\widgets\Login;
 use app\modules\shop\widgets\Catalog;
 use app\modules\shop\widgets\Search;
-
+use yii\easyii\modules\text\api\Text;
 use app\modules\news\api\News;
 
 /**
@@ -133,38 +133,7 @@ $asset = \app\themes\rusena\assets\Asset::register($this);
 			<div class="row">
 				<div class="col-md-4">
 					<a id="footer-logo" href="/">Русена</a>
-                    <!-- Yandex.Metrika informer -->
-                    <a href="https://metrika.yandex.ru/stat/?id=30752153&amp;from=informer"
-                       target="_blank" rel="nofollow"><img src="//bs.yandex.ru/informer/30752153/3_1_FF31A8FF_E61188FF_0_pageviews"
-                                                           style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" onclick="try{Ya.Metrika.informer({i:this,id:30752153,lang:'ru'});return false}catch(e){}"/></a>
-                    <!-- /Yandex.Metrika informer -->
-
-                    <!-- Yandex.Metrika counter -->
-                    <script type="text/javascript">
-                        (function (d, w, c) {
-                            (w[c] = w[c] || []).push(function() {
-                                try {
-                                    w.yaCounter30752153 = new Ya.Metrika({id:30752153,
-                                        clickmap:true,
-                                        trackLinks:true,
-                                        accurateTrackBounce:true});
-                                } catch(e) { }
-                            });
-
-                            var n = d.getElementsByTagName("script")[0],
-                                s = d.createElement("script"),
-                                f = function () { n.parentNode.insertBefore(s, n); };
-                            s.type = "text/javascript";
-                            s.async = true;
-                            s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
-
-                            if (w.opera == "[object Opera]") {
-                                d.addEventListener("DOMContentLoaded", f, false);
-                            } else { f(); }
-                        })(document, window, "yandex_metrika_callbacks");
-                    </script>
-                    <noscript><div><img src="//mc.yandex.ru/watch/30752153" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-                    <!-- /Yandex.Metrika counter -->
+                    <?=Text::get('metrika')?>
 				</div>
 				<div class="col-md-4">
 					<div id="nav-footer" class="footer-menu">
